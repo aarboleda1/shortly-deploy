d6158913a3bfec9aa5314485974af306b0bc7e54
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 19:22:12 -0800

    Implement user Mongo schema and model This commit also adds: - refactor signup and login features - refactor hashPassword and comparePasswords methods

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,32 @@
+var mongoose = require('mongoose');
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+db.users = mongoose.Schema({
+  username: String,
+  password: String
+}, { timestamps: true});
+
+db.users.methods.comparePassword = function (attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(err, isMatch);
+  });
+};
+
+db.users.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) { return next(); }
+  var cipher = Promise.promisify(bcrypt.hash);
+  cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      return hash;
+    }).then(function(hash) {
+      this.password = hash;
+      next();
     });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
 });
+
+var User = mongoose.model('User', db.users);
 
 module.exports = User;
