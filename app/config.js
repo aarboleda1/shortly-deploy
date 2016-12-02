f10e6153833f828b5c257ae7f2e4d921b0c0bc94
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 20:26:59 -0800

    Implements Mongo urls schema and model and refactors request handling methods

--- app/config.js
+++ app/config.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/shortly');
 
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
